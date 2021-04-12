38030ecd3cddf672ca7a8e87a02e9760877dff05
Francis Hilaire <Prometee@users.noreply.github.com>
2021-04-12 17:20:18 +0200

    Remove selenium support and focus ecs on src folder

--- ecs.php
+++ ecs.php
@@ -3,18 +3,7 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/vendor/sylius-labs/coding-standard/ecs.php');
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::SKIP, [
-        __DIR__ . '/vendor/*',
-        __DIR__ . '/node_modules/*',
-        __DIR__ . '/tests/Application/config/*/bundles.php',
-        __DIR__ . '/tests/Application/config/bundles.php',
-        __DIR__ . '/tests/Application/node_modules/*',
-        __DIR__ . '/tests/Application/var/*',
-    ]);
 };
--- tests/Application/config/symfony/5.2/bundles.php
+++ tests/Application/config/symfony/5.2/bundles.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symplify\ConsoleColorDiff\Bundle\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
 ];
