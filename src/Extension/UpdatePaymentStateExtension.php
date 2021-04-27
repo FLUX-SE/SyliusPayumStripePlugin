18a0bf05a63966b6ab771e8433261f4ede688524
Francis Hilaire <Prometee@users.noreply.github.com>
2021-04-27 11:19:09 +0200

    Add replacement for the old UpdatePaymentStateExtension of Sylius using Payum Core StorageExtension behaviour

--- src/Extension/UpdatePaymentStateExtension.php
+++ src/Extension/UpdatePaymentStateExtension.php
@@ -4,95 +4,144 @@ declare(strict_types=1);
 
 namespace FluxSE\SyliusPayumStripePlugin\Extension;
 
-use ArrayAccess;
-use FluxSE\PayumStripe\Action\Api\WebhookEvent\AbstractPaymentAction;
-use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\Extension\Context;
 use Payum\Core\Extension\ExtensionInterface;
-use Payum\Core\Request\Notify;
-use Payum\Core\Security\TokenInterface;
+use Payum\Core\Model\ModelAggregateInterface;
+use Payum\Core\Storage\IdentityInterface;
+use Payum\Core\Storage\StorageInterface;
 use SM\Factory\FactoryInterface;
+use SM\SMException;
 use Sylius\Bundle\PayumBundle\Request\GetStatus;
 use Sylius\Component\Payment\Model\PaymentInterface;
 use Sylius\Component\Payment\PaymentTransitions;
 use Sylius\Component\Resource\StateMachine\StateMachineInterface;
 use Webmozart\Assert\Assert;
 
+/**
+ * Reproduction of \Payum\Core\Extension\StorageExtension for Sylius payments
+ */
 final class UpdatePaymentStateExtension implements ExtensionInterface
 {
     /** @var FactoryInterface */
     private $factory;
 
-    public function __construct(FactoryInterface $factory)
+    /**
+     * @var StorageInterface
+     */
+    protected $storage;
+
+    /** @var PaymentInterface[] */
+    private $scheduledPaymentsToProcess;
+
+    public function __construct(FactoryInterface $factory, StorageInterface $storage)
     {
         $this->factory = $factory;
+        $this->storage = $storage;
+        $this->scheduledPaymentsToProcess = [];
     }
 
     public function onPreExecute(Context $context): void
     {
+        /** @var mixed|ModelAggregateInterface $request */
+        $request = $context->getRequest();
+
+        if (false === $request instanceof ModelAggregateInterface) {
+            return;
+        }
+
+        if ($request->getModel() instanceof IdentityInterface) {
+            $payment = $this->storage->find($request->getModel());
+        } else {
+            /** @var PaymentInterface|mixed $payment */
+            $payment = $request->getModel();
+        }
+
+        if (false === $payment instanceof PaymentInterface) {
+            return;
+        }
+
+        $this->scheduleForProcessingIfSupported($payment);
     }
 
     public function onExecute(Context $context): void
     {
     }
 
+    /**
+     * @throws SMException
+     */
     public function onPostExecute(Context $context): void
     {
-        if (null !== $context->getException()) {
-            return;
-        }
-
+        /** @var mixed|ModelAggregateInterface $request */
         $request = $context->getRequest();
 
-        if (false === $request instanceof Notify) {
-            return;
-        }
-
-        $token = $request->getToken();
-        if (false === $token instanceof TokenInterface) {
-            return;
-        }
-
-        $webhookPayment = false;
-        foreach ($context->getPrevious() as $prevContext) {
-            if ($prevContext->getAction() instanceof AbstractPaymentAction) {
-                $webhookPayment = true;
-                break;
+        if ($request instanceof ModelAggregateInterface) {
+            /** @var PaymentInterface|mixed $payment */
+            $payment = $request->getModel();
+            if ($payment instanceof PaymentInterface) {
+                $this->scheduleForProcessingIfSupported($payment);
             }
         }
 
-        if (false === $webhookPayment) {
+        if (count($context->getPrevious()) > 0) {
             return;
         }
 
-        $details = $request->getModel();
-        if (!$details instanceof ArrayObject) {
-            return;
-        }
-
-        $payment = $request->getFirstModel();
-        if (false === $payment instanceof PaymentInterface) {
-            return;
-        }
-
-        $payment->setDetails($details->getArrayCopy());
-
-        $context->getGateway()->execute($status = new GetStatus($payment));
-        /** @var string $value */
-        $value = $status->getValue();
-        if ($payment->getState() !== $value && PaymentInterface::STATE_UNKNOWN !== $value) {
-            $this->updatePaymentState($payment, $value);
+        // Process scheduled payments only when we are post executing a
+        // root payum request
+        foreach ($this->scheduledPaymentsToProcess as $id => $payment) {
+            $this->processPayment($context, $payment);
+            unset($this->scheduledPaymentsToProcess[$id]);
         }
     }
 
+    /**
+     * @throws SMException
+     */
+    private function processPayment(Context $context, PaymentInterface $payment): void
+    {
+        $status = new GetStatus($payment);
+        $context->getGateway()->execute($status);
+        $value = (string) $status->getValue();
+        if ($payment->getState() === $value) {
+            return;
+        }
+
+        if (PaymentInterface::STATE_UNKNOWN === $value) {
+            return;
+        }
+
+        $this->updatePaymentState($payment, $value);
+    }
+
+    /**
+     * @throws SMException
+     */
     private function updatePaymentState(PaymentInterface $payment, string $nextState): void
     {
         $stateMachine = $this->factory->get($payment, PaymentTransitions::GRAPH);
 
         Assert::isInstanceOf($stateMachine, StateMachineInterface::class);
 
-        if (null !== $transition = $stateMachine->getTransitionToState($nextState)) {
-            $stateMachine->apply($transition);
+        $transition = $stateMachine->getTransitionToState($nextState);
+        if (null === $transition) {
+            return;
         }
+
+        $stateMachine->apply($transition);
+    }
+
+    private function scheduleForProcessingIfSupported(PaymentInterface $payment): void
+    {
+        $id = $payment->getId();
+        if (null === $id) {
+            return;
+        }
+
+        if (false === is_int($id)) {
+            return;
+        }
+
+        $this->scheduledPaymentsToProcess[$id] = $payment;
     }
 }
