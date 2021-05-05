cd016581cb18c2a203accb84dbcdf342730c1d77
Francis Hilaire <Prometee@users.noreply.github.com>
2021-05-05 13:38:49 +0200

    Fix ecs issue

--- ecs.php
+++ ecs.php
@@ -2,8 +2,18 @@
 
 declare(strict_types=1);
 
+use PhpCsFixer\Fixer\Operator\BinaryOperatorSpacesFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $containerConfigurator->import(__DIR__ . '/vendor/sylius-labs/coding-standard/ecs.php');
+
+    /**
+     * Was added to fix this exception:
+     *
+     * PhpCsFixer\ConfigurationException\InvalidFixerConfigurationException: [binary_operator_spaces] Invalid configuration:
+     * The options "align_double_arrow", "align_equals" do not exist. Defined options are: "default", "operators".
+     * in vendor/friendsofphp/php-cs-fixer/src/AbstractFixer.php on line 155
+     */
+    $containerConfigurator->services()->set(BinaryOperatorSpacesFixer::class);
 };
