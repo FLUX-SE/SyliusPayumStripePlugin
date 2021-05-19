5e04572e7b3e7e22cf66629da34f1161ff399fdd
Francis Hilaire <Prometee@users.noreply.github.com>
2021-05-19 12:24:51 +0200

    Remove ConsoleColorDiffBundle bundle

--- tests/Application/config/symfony/4.4/bundles.php
+++ tests/Application/config/symfony/4.4/bundles.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 return [
-    Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
+    //Symplify\ConsoleColorDiff\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
 ];
--- tests/Application/config/symfony/5.2/bundles.php
+++ tests/Application/config/symfony/5.2/bundles.php
@@ -3,5 +3,5 @@
 declare(strict_types=1);
 
 return [
-    Symplify\ConsoleColorDiff\Bundle\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
+    //Symplify\ConsoleColorDiff\Bundle\ConsoleColorDiffBundle::class => ['dev' => true, 'test' => true],
 ];
