a731ee50bfc1fea0710330c90a7e9929e4efb27a
Francis Hilaire <Prometee@users.noreply.github.com>
2024-06-25 13:47:54 +0200

    Fix deprecation

--- src/Form/Type/StripeCheckoutSessionGatewayConfigurationType.php
+++ src/Form/Type/StripeCheckoutSessionGatewayConfigurationType.php
@@ -23,7 +23,7 @@ class StripeCheckoutSessionGatewayConfigurationType extends AbstractType
         );
     }
 
-    public function getParent()
+    public function getParent(): ?string
     {
         return StripeGatewayConfigurationType::class;
     }
