308ab3799c5bd230ed5464f9d1aecc2626f184b6
Francis Hilaire <Prometee@users.noreply.github.com>
2024-09-26 11:17:08 +0200

    Update ECS config

--- ecs.php
+++ ecs.php
@@ -5,19 +5,15 @@ declare(strict_types=1);
 use PhpCsFixer\Fixer\ControlStructure\TrailingCommaInMultilineFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ECSConfig $ecsConfig): void {
-    $ecsConfig->import(__DIR__ . '/vendor/sylius-labs/coding-standard/ecs.php');
+return static function (ECSConfig $config): void {
+    $config->import(__DIR__ . '/vendor/sylius-labs/coding-standard/ecs.php');
 
-    $ecsConfig->paths([
+    $config->paths([
         'src',
         'tests/Behat',
     ]);
 
-    $services = $ecsConfig->services();
-
     // PHP 7 compatibility
-    $services
-        ->set(TrailingCommaInMultilineFixer::class)
-        ->call('configure', [['elements' => ['arrays']]])
-    ;
+    $config->ruleWithConfiguration(TrailingCommaInMultilineFixer::class, ['elements' => ['arrays']]);
+
 };
