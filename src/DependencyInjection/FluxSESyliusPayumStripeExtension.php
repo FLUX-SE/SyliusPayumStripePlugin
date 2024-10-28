abd682bfc282bddbb1a1c23e2e1bb20c20a4f628
Francis Hilaire <Prometee@users.noreply.github.com>
2024-10-28 10:14:17 +0100

    Move specific loading to the preprend method

--- src/DependencyInjection/FluxSESyliusPayumStripeExtension.php
+++ src/DependencyInjection/FluxSESyliusPayumStripeExtension.php
@@ -8,9 +8,10 @@ use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class FluxSESyliusPayumStripeExtension extends Extension
+class FluxSESyliusPayumStripeExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * @throws Exception
@@ -47,6 +48,14 @@ class FluxSESyliusPayumStripeExtension extends Extension
             new FileLocator(dirname(__DIR__) . '/Resources/config'),
         );
         $loader->load('services.yaml');
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(dirname(__DIR__) . '/Resources/config'),
+        );
 
         if ($container->hasExtension('sylius_state_machine_abstraction')) {
             $loader->load('services/abstraction/abstraction.yaml');
