f9229f23032d9d168248752bce8b6d759dc2f242
Francis Hilaire <Prometee@users.noreply.github.com>
2025-06-12 13:27:30 +0200

    Switch to test application

---
+++ tests/TestApplication/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    FluxSE\PayumStripeBundle\FluxSEPayumStripeBundle::class => ['all' => true],
+    FluxSE\SyliusPayumStripePlugin\FluxSESyliusPayumStripePlugin::class => ['all' => true],
+];
